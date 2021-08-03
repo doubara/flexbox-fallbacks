20cd16ba83bd8f8c02a567574768d301f97cf5d2
doubara <70749551+doubara@users.noreply.github.com>
2021-08-03 14:22:58 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Flexbox Fallbacks</title>
+  
+  <!-- HTML -->
+  
+
+  <!-- Custom Styles -->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <!-- Without Any Justification -->
+  <div>
+    <section>
+      <h1>Without justified content or align items</h1>
+      <nav class="contain container container-x">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+    <section>
+      <h1>Without flexbox</h1>
+      <nav class="contain no-flex">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+  </div>
+  <!-- Justify content flex end -->
+  <div>
+    <section>
+      <h1>Justify content flex end and align items flex end</h1>
+      <nav class="contain container container-1">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+    <section>
+      <h1>Fall back: margin left 25%(parent-width/4) of first child</h1>
+      <nav class="contain no-flex no-flex-1">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+  </div>
+  <!-- Justify content center  -->
+  <div>
+    <section>
+      <h1>Justify content center</h1>
+      <nav class="contain container container-2">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+    <section>
+      <h1>Fall back: first element left margin to 12.5%</h1>
+      <nav class="contain no-flex no-flex-2">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+  </div>
+  <!-- justify content space between -->
+  <div>
+    <section>
+      <h1>Justify content space between</h1>
+      <nav class="contain container container-3">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+    <section>
+      <h1>Fall back: set every div that isnt the first div a margin left of 12.5%</h1>
+      <nav class="contain no-flex no-flex-3">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+  </div>
+  <!-- justify content flex around -->
+  <div>
+    <section>
+      <h1>Justify content space around</h1>
+      <nav class="contain container container-4">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+    <section>
+      <h1>Fall back: margin of 12.5% to every child</h1>
+      <nav class="contain no-flex no-flex-4">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+  </div>
+  <!-- justify content space evenly  -->
+  <div>
+    <section>
+      <h1>Justify content space evenly</h1>
+      <nav class="contain container container-5">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+    <section>
+      <h1>Fall back: margin of 6.25% to first and last child</h1>
+      <nav class="contain no-flex no-flex-5">
+        <div class="home">1 Home</div>
+        <div class="search">2 Search</div>
+        <div class="logout">3 Logout</div>
+      </nav>
+    </section>
+  </div>
+  <script src="main.js"></script>
+</body>
+</html>
